9806c47ac367065cd00d4a10ef121fb897930c48
iker catalan gomez <100007043.joan23@fje.edu>
2022-05-11 16:40:56 +0200

    login hecho

---
+++ proc/proc.login.php
@@ -0,0 +1,47 @@
+<?php
+// recuperas las variables
+$mail = $_POST['mail'];
+echo $mail;
+$contra = $_POST['contra'];
+echo $contra;
+$conexion = mysqli_connect('localhost','root','','bd_javiker');
+$sql = "SELECT * FROM tbl_registros WHERE correo = '$mail' && contraseña = $contra";
+$correos = mysqli_query($conexion,$sql);
+
+foreach ($correos as $id) {
+    if ($id['correo'] == $mail) {
+        session_start();
+        $_SESSION['id'] = $id['id'];
+        ?>
+        <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+        <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+        <script>
+            Swal.fire({
+            icon: 'success',
+            title: 'Usuario correcto',
+            text: 'Usuario o contraseña equivocados!',
+            showConfirmButton: false,
+            footer: '<a href="../view/actividades.html"></a>'
+          })
+         </script>
+         <?php
+    }else {
+        ?>
+         <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+         <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+         <script>
+            Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Usuario o contraseña equivocados!',
+            footer: '<a href="../login/login.html"></a>'
+          })
+         </script>
+
+        <?php
+    }
+}
+
+
+
+
